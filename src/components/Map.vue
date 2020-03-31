bab269a3de83d0d2800b000ef091464c041f466e
havillalobos331 <horacio@partnergrammer.com>
2020-03-31 09:28:34 -0700

    centrar el mapa en chihuahua

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,7 @@
 <template>
 
-  <div style="height: 500px; width: 100%">
-    <div style="height: 200px overflow: auto;">
+  <div style="height: 800px; width: 100%">
+    <div style="height: 100% overflow: auto;">
       <p>First marker is placed at {{ withPopup.lat }}, {{ withPopup.lng }}</p>
       <p>Center is at {{ currentCenter }} and the zoom is: {{ currentZoom }}</p>
       <button @click="showLongText">
@@ -67,15 +67,15 @@ export default {
   },
   data () {
     return {
-      zoom: 13,
-      center: latLng(47.41322, -1.219482),
+      zoom: 7,
+      center: latLng(28.407398, -106.99157),
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      withPopup: latLng(47.41322, -1.219482),
-      withTooltip: latLng(47.41422, -1.250482),
+      withPopup: latLng(28.7387113, -105.7619195),
+      withTooltip: latLng(28.7387113, -108.7619195),
       currentZoom: 11.5,
-      currentCenter: latLng(47.41322, -1.219482),
+      currentCenter: latLng(28.407398, -106.99157),
       showParagraph: false,
       mapOptions: {
         zoomSnap: 0.5
